17c0b54533f93bb01af61bf616c181b5308d1c0b
Dominik Cebula <dominikcebula@gmail.com>
2025-04-04 23:02:05 +0200

    Hexagonal Architecture Sample - Loan Application and Approval System

--- src/main/java/com/dominikcebula/samples/loans/application/domain/model/person/Age.java
+++ src/main/java/com/dominikcebula/samples/loans/application/domain/model/person/Age.java
@@ -8,12 +8,15 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 public class Age {
+    private static final int MIN_AGE = 0;
+    private static final int MAX_AGE = 120;
+
     private final int value;
 
     public Age(int value) {
-        if (value < 0)
+        if (value < MIN_AGE)
             throw new IllegalArgumentException("Age cannot be negative.");
-        if (value > 120)
+        if (value > MAX_AGE)
             throw new IllegalArgumentException("Age cannot be greater than 120.");
 
         this.value = value;
