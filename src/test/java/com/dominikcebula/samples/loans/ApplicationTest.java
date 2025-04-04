ff9400f4beacb9ec818ac19390e8a884acb908e8
Dominik Cebula <dominikcebula@gmail.com>
2025-04-04 21:52:55 +0200

    Hexagonal Architecture Sample - Loan Application and Approval System

---
+++ src/test/java/com/dominikcebula/samples/loans/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.dominikcebula.samples.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
