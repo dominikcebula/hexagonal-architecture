02d2f2b49a818c95e00f36ed50dff5236c7a4213
Dominik Cebula <dominikcebula@gmail.com>
2025-04-15 21:07:14 +0200

    Hexagonal Architecture Sample - Loan Application and Approval System

--- src/main/java/com/dominikcebula/samples/loans/adapter/out/persistence/LoanApplicationRepositoryAdapter.java
+++ src/main/java/com/dominikcebula/samples/loans/adapter/out/persistence/LoanApplicationRepositoryAdapter.java
@@ -2,9 +2,11 @@ package com.dominikcebula.samples.loans.adapter.out.persistence;
 
 import com.dominikcebula.samples.loans.application.domain.model.loan.LoanApplication;
 import com.dominikcebula.samples.loans.application.port.out.persistence.LoanApplicationRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public class LoanApplicationRepositoryAdapter implements LoanApplicationRepository {
     @Override
     public Optional<LoanApplication> findById(int id) {
