01ccff90728b2d66ed50ea90171832177dd51830
Dominik Cebula <dominikcebula@gmail.com>
2025-04-15 21:51:02 +0200

    Hexagonal Architecture Sample - Loan Application and Approval System

--- src/main/resources/db/changelog/db.changelog-001-init-db.sql
+++ src/main/resources/db/changelog/db.changelog-001-init-db.sql
@@ -1,1 +1,14 @@
--- TODO
+CREATE SEQUENCE loan_applications_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE CACHE 1;
+
+CREATE TABLE loan_applications
+(
+    id              BIGINT PRIMARY KEY DEFAULT nextval('loan_applications_seq'),
+    amount          NUMERIC,
+    currency        VARCHAR(3),
+    terms_in_months INTEGER,
+    status          VARCHAR(10)
+);
