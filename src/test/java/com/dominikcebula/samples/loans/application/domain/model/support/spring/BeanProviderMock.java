c15ec509f8ccc6b6c34c6105417156a2bfbcbc69
Dominik Cebula <dominikcebula@gmail.com>
2025-05-18 20:05:02 +0200

    Hexagonal Architecture Sample - Loan Application and Approval System

--- src/test/java/com/dominikcebula/samples/loans/application/domain/model/support/spring/BeanProviderMock.java
+++ src/test/java/com/dominikcebula/samples/loans/application/domain/model/support/spring/BeanProviderMock.java
@@ -1,19 +1,28 @@
 package com.dominikcebula.samples.loans.application.domain.model.support.spring;
 
-import org.mockito.Mockito;
 import org.springframework.beans.factory.BeanFactory;
 
+import static org.mockito.Mockito.*;
+
 public class BeanProviderMock {
+    private static BeanFactory oldBeanFactory;
+
     public static void setUp() {
-        if (BeanProvider.beanFactory == null)
-            BeanProvider.beanFactory = Mockito.mock(BeanFactory.class);
+        if (BeanProvider.beanFactory == null || notMockedYet()) {
+            oldBeanFactory = BeanProvider.beanFactory;
+            BeanProvider.beanFactory = mock(BeanFactory.class);
+        }
     }
 
     public static <T> void registerBean(Class<T> type, T bean) {
-        Mockito.when(BeanProvider.beanFactory.getBean(type)).thenReturn(bean);
+        when(BeanProvider.beanFactory.getBean(type)).thenReturn(bean);
     }
 
     public static void tearDown() {
-        BeanProvider.beanFactory = null;
+        BeanProvider.beanFactory = oldBeanFactory;
+    }
+
+    private static boolean notMockedYet() {
+        return !mockingDetails(BeanProvider.beanFactory).isMock();
     }
 }
